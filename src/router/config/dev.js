3c0718541e36a472706b746e8c8d09bf07c21ed8
xjyplayer <2532917800@qq.com>
2021-05-05 16:08:02 +0800

    init

--- src/router/config/dev.js
+++ src/router/config/dev.js
@@ -1,10 +1,10 @@
 
 var apiConfig = {
-  file_api_host: 'http://file.store.k8s.show',
+  file_api_host: 'http://file-store.k8s.show',
   file_api_port: '80',
   file_api_prefix: '/api/file/',
 
-  user_api_host: 'http://file.store.k8s.show',
+  user_api_host: 'http://file-store.k8s.show',
   user_api_port: '80',
   user_api_prefix: '/api/user/',
 
